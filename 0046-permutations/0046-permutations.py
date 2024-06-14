4ab1e33aaa1cf5cfccf74019066ab8ba25ca91bc
Eric Musselman, PhD <edm23@duke.edu>
2024-06-13 17:39:47 -0700

    Time: 26 ms (19.16%), Space: 11.7 MB (71.30%) - LeetHub

---
+++ 0046-permutations/0046-permutations.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        def backtrack(curr):
+            if len(curr) == len(nums):
+                output.append(curr[:])
+                return
+        
+            for num in nums:
+                if num not in curr:
+                    curr.append(num)
+                    backtrack(curr)
+                    curr.pop()
+            
+        output = []
+        backtrack([])
+        return output
