11c7a9e24a901ffe8023419b1823da9971a79de7
Eric Musselman, PhD <edm23@duke.edu>
2024-07-01 12:07:26 -0700

    Time: 84 ms (9.26%), Space: 19.8 MB (37.60%) - LeetHub

---
+++ 0252-meeting-rooms/0252-meeting-rooms.py
@@ -0,0 +1,15 @@
+class Solution:
+    def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
+        
+        if len(intervals) == 1:
+            return True
+        
+        intervals.sort()
+        
+        for idx in range(len(intervals)-1):
+            if intervals[idx+1][0] < intervals[idx][1]:
+                return False
+        return True
+    
+    # Time: O(n), Space: O(n)
+        
