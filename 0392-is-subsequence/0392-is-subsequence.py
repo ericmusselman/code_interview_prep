458a902bdedb591233891576291957fd8e2ed29e
Eric Musselman, PhD <edm23@duke.edu>
2024-07-14 12:47:06 -0800

    Time: 31 ms (85.05%), Space: 16.7 MB (7.51%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        
+        si = 0
+        ti = 0
+        while si < len(s) and ti < len(t):
+            if s[si] == t[ti]:
+                si += 1
+            ti += 1
+        
+        return si == len(s)
+    
+    # Time: O(n), Space: O(1)
+    
